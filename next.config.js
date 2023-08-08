4f777f6acb66227d7e1f3b5f00a82428c866da62
Ardhi Choiruddin <33990014+ardhichoiruddin@users.noreply.github.com>
2023-08-08 18:24:32 +0700

    add header at next config

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
         hostname: "penak966616533.files.wordpress.com",
       },
       {
@@ -16,6 +12,27 @@ const nextConfig = {
         hostname: "penak966616533.files.wordpress.com",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: process.env.DOMAIN },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+    ];
   },
 };
 
