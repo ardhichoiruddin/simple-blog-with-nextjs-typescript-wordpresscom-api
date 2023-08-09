ebee4b15e7e8bc4c6cd6aaf4e7eb5532dea1a671
Ardhi Choiruddin <33990014+ardhichoiruddin@users.noreply.github.com>
2023-08-09 21:22:52 +0700

    add ga analytic and nprogress

---
+++ src/libs/gtag/index.ts
@@ -0,0 +1,21 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
+
+export const pageview = (url: any) => {
+  window?.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+export const event = ({ action, category, label, value }: any) => {
+  window?.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
+import { useEffect } from "react";
 import type { NextPage } from "next";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import Router from "next/router";
 
 import "../../public/assets/icons/iconmoon/style.css";
+import "nprogress/nprogress.css";
+import * as gtag from "@/libs/gtag";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,9 +22,51 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   const getLayout = Component?.getLayout ?? ((page) => page);
+
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', "${process.env.NEXT_PUBLIC_GA_ID}");
+        `}
+      </Script>
       <ThemeProvider attribute="class">
         {getLayout(<Component {...pageProps} />)}
       </ThemeProvider>
